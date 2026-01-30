b6b6ae22610ed4fe34ee1a3edbc97d23298c5805
faezsal <faez_sal@yahoo.com>
2026-01-30 01:28:20 +0100

    Add ProblemDetailsExtension for DI configuration
    Introduces the ProblemDetailsExtension class to load service definitions from the config/services.php file, enabling dependency injection setup for the bundle.

---
+++ src/DependencyInjection/ProblemDetailsExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Faez84\ProblemDetailsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+final class ProblemDetailsExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.php');
+    }
+}
